4faaf7cdaf2d5054919db209ae071565eae69694
Richard Wohlbold <richard@rgwohlbold.de>
2022-10-24 11:20:25 +0200

    Implement create in author_controller.rb

--- app/controllers/author_controller.rb
+++ app/controllers/author_controller.rb
@@ -6,4 +6,13 @@ class AuthorController < ApplicationController
     def show
         @author = Author.find(params[:id])
     end
+
+    def create
+        @author = Author.new(params.require(:author).permit(:first_name, :last_name, :homepage))
+        if @author.save
+            redirect_to root_path, notice: 'Success!'
+        else
+            render :new
+        end
+    end
 end
