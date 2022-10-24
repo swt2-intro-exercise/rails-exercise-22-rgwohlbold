daf2d346432b489b3225845813e7a431ddf5ae55
Richard Wohlbold <richard@rgwohlbold.de>
2022-10-24 15:52:52 +0200

    Scaffold paper

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_24_081119) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_24_135104) do
   create_table "authors", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
     t.string "homepage"
+  end
+
+  create_table "papers", force: :cascade do |t|
+    t.string "title"
+    t.string "venue"
+    t.integer "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
