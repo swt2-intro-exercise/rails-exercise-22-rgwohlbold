a12675cfc9e69de342fb33250dc3b79b8afece21
Richard Wohlbold <richard@rgwohlbold.de>
2022-10-24 16:23:27 +0200

    Render author on paper page

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_24_135104) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_24_141217) do
   create_table "authors", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
     t.string "homepage"
+  end
+
+  create_table "authors_papers", force: :cascade do |t|
+    t.integer "author_id"
+    t.integer "paper_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "papers", force: :cascade do |t|
